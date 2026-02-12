1653f97dea6beb2767f13bba7cae0d3160a0ec8d
Gopichand Reddy <62110262+Gopichand22@users.noreply.github.com>
2026-02-12 18:19:10 +0530

    Add responsive typewriter wrappers for mobile/desktop
    Wrap each Typewriter instance with desktop/tab wrapper and add a mobile-only Typewriter variant in src/components/Home/Type.js to display a condensed/different set of strings and delete speed on small screens. Update src/style.css to hide the mobile wrapper by default and toggle visibility via a media query (show mobile wrapper and hide desktop/tab wrappers under 767px). This improves layout and content presentation across screen sizes.

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -4,6 +4,7 @@ import Typewriter from "typewriter-effect";
 function Type() {
   return (
   <div>
+  <div className="typewriter-wrapper-desktoptab">
     <Typewriter
       options={{
         strings: [
@@ -17,6 +18,8 @@ function Type() {
         deleteSpeed: 100,
       }}
     />
+    </div>
+    <div className="typewriter-wrapper-desktoptab">
     <Typewriter
       options={{
         strings: [
@@ -30,6 +33,8 @@ function Type() {
         deleteSpeed: 90,
       }}
     />
+    </div>
+    <div className="typewriter-wrapper-desktoptab">
     <Typewriter
       options={{
         strings: [
@@ -43,6 +48,31 @@ function Type() {
         deleteSpeed: 70,
       }}
     />
+    </div>
+    <div className="typewriter-wrapper-mobile">
+      <Typewriter
+        options={{
+          strings: [
+          "AEM Full Stack Developer",
+            "Expertise in AEM 6.4 | 6.5 | AEMaaCS",
+            "AEM Sites | AEM Assets | AEM EDS Franklin",
+            "AEM Add-On's: Dynamic Media | Firefly Assets",
+            "Npm, webpack, sass",
+            "Schedulers, Servlets, REST APIs, Jobs, Event Listeners",
+            "Git, Maven, Eclipse / IntelliJ, Felix",
+            "Basic Python for offline utils",
+            "Sling Models, OSGi Configs & Services",
+          "JSP → HTL (Sightly) Migration",
+          "Dialogs, Templates, Touch UI",
+          "Java, HTML, CSS + (Logical CSS), JavaScript, jQuery, Groovy Expert",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 70,
+        }}
+      />
+    </div>
+
     </div>
   );
 }
